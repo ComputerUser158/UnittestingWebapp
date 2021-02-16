3733f12f98b8d8dba4af3ee1b72616ca5153d018
ComputerUser158 <oneringraw@gmail.com>
2021-02-15 20:43:14 -0600

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace AgeUnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            /* I really don't know how untesting works when you include it with web apps
+             * I understand basic format for console app unit tests, but web apps add another layer
+             * of difficulty I'm not equipped to deal with. */
+        }
+    }
+}
